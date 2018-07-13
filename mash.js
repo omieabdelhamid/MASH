4869679d44c9cf976d4ffe0a6f4274cbecaedb5e
omieabdelhamid <stopmotionomar@gmail.com>
2018-07-13 14:10:29 -0400

    yuh

---
+++ mash.js
@@ -0,0 +1,22 @@
+let wife = ["Jennifer McKenzie(Star of Black Beauty)", " Romar and Naz (Polyamorous)", "Hilary Clinton and Melania Trump (Polyamorous)", "Jennifer Lopez","that Girl that Curved Ian"]
+let job = ["Rollercoaster Engineer", " Drug Dealer", " Vlogger/Rapper", "a hater like Sutherland Brown", "Computer Scientist" ]
+let car = ["Toyota Camary", "NYC Train", "Bike", "Jaguar", "Pattywagon"]
+let house = ["Cardboard Box", "Wall Street Condo", "Penthouse on Both Coasts, PH Balance", "Shack on the beach", "Cave"]
+let shoes = ["New Balance", "Margielas" , "Yeezys", "Cardboard Boxes", "Sperrys" ]
+let BatMitsvahPerformer = ["6ix9ine", "Rebecca Black", "A Boogie", "Kanye", "Drake"]
+let 
+
+let yurr = wife[Math.floor( 5 * Math.random())];
+let yurr2 = job[Math.floor( 5 * Math.random())];
+let yurr3 = car[Math.floor( 5 * Math.random())];
+let yurr4 = house[Math.floor( 5 * Math.random())];
+let yurr5 = shoes[Math.floor( 5 * Math.random())];
+let yurr6 = BatMitsvahPerformer[Math.floor( 5 * Math.random())];
+
+
+console.log("You will marry " + yurr);
+console.log("You will work as a " + yurr2);
+console.log ("You will pull up in a " + yurr3);
+console.log ("You will live in a " + yurr4);
+console.log ("You will wear " + yurr5);
+console.log (yurr6 + " will perform at your daughter's Bat Mitsvah. Shalom!");
